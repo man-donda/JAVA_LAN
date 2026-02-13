62d582eb822b8381d8dc28344c85f541504059a1
man-donda <mann.9dot@gmail.com>
2026-02-13 17:54:37 +0530

    add tut38 programs

---
+++ APNA_COLLEGE/tempCodeRunnerFile.java
@@ -0,0 +1,7 @@
+public Node() {
+        //     children = new Node[26];
+        //     for (int i = 0; i < 26; i++) {
+        //         children[i] = null;
+        //     }
+        //     eow = false;
+        // }
---
+++ APNA_COLLEGE/tut38_TRIE_DATA_STRUCTURE.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class tut38_TRIE_DATA_STRUCTURE {
+    static class Node {
+        Node[] children;
+        boolean eow;
+
+        public Node() {
+            children = new Node[26];
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+            eow = false;
+        }
+    }
+
+    static Node root = new Node();
+
+    //------------------- Insert words in Trie ---------------------
+    public static void insert(String word){
+        Node curr = root;
+        for(int i = 0; i < word.length(); i++){
+            int idx = word.charAt(i)-'a';
+
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            }
+
+            if(i == word.length()-1){
+                curr.children[idx].eow = true;
+            }
+            curr = curr.children[idx];
+        }
+    }
+
+    //------------------- Search words in Trie ---------------------
+    public static boolean search(String key){
+        Node curr = root;
+        for(int i = 0; i < key.length(); i++){
+            int idx = key.charAt(i)-'a';
+            Node node = curr.children[idx];
+
+            if(node == null){
+                return false;
+            }
+
+            if(i == key.length()-1 && node.eow == false){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String words[] = { "the", "a", "there", "their", "any" };
+        for(int i = 0; i < words.length; i++){
+            insert(words[i]);
+        }
+
+        System.out.println(search("their"));
+        System.out.println(search("thor"));
+        System.out.println(search("an"));
+    }
+}
