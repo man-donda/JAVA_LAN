b4361e91c71d8f1b21bf0c14bcda7cadd0520f71
man-donda <mann.9dot@gmail.com>
2026-03-13 16:44:08 +0530

    Clear repository

--- JAVA_PROGRAMMING/APNA_COLLEGE/tut02_MainClass.java
+++
@@ -1,9 +0,0 @@
-public class tut02_MainClass {
-    public static void main(String[] args) {
-        //output
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
-    }
-}
